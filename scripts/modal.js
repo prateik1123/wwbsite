b6638956eb2678cd2f6a151793c25b49c1749d37
Pavel <stegaspasha@gmail.com>
2020-05-15 18:15:50 +0300

    -

---
+++ scripts/modal.js
@@ -0,0 +1,19 @@
+const modal = document.getElementsByClassName("modal-start");
+const modalCheck = document.getElementsByClassName("modal-check");
+
+function openModal() {
+	modal[0].classList.add('is-active')
+}
+
+function closeModal() {
+	modal[0].classList.remove('is-active')
+}
+
+
+function openModalCheck() {
+	modalCheck[0].classList.add('is-active')
+}
+
+function closeModalCheck() {
+	modalCheck[0].classList.remove('is-active')
+}
