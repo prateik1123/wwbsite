5f37ccdf77c92b8b76375084fd055a10689303b5
Pavel <stegaspasha@gmail.com>
2020-05-21 14:26:10 +0300

    3

--- scripts/header.js
+++ scripts/header.js
@@ -7,3 +7,24 @@ window.onscroll = function () {
 	}
 
 };
+
+document.addEventListener('DOMContentLoaded', () => {
+
+	const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+	if ($navbarBurgers.length > 0) {
+
+		$navbarBurgers.forEach( el => {
+			el.addEventListener('click', () => {
+
+				const target = el.dataset.target;
+				const $target = document.getElementById(target);
+
+				el.classList.toggle('is-active');
+				$target.classList.toggle('is-active');
+
+			});
+		});
+	}
+
+});
