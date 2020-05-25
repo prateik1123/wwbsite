64649a5946a845dca8bc2147086326403ed3068c
Pavel <stegaspasha@gmail.com>
2020-05-25 18:24:26 +0300

    123

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,19 +1,24 @@
 const modal = document.querySelectorAll(".modal-start");
 const modalCheck = document.getElementsByClassName("modal-check");
+const html = document.querySelector('html');
 
 function openModal() {
-	modal[0].classList.add('is-active')
+	modal[0].classList.add('is-active');
+	html.classList.add('scroll-hidden');
 }
 
 function closeModal() {
-	modal[0].classList.remove('is-active')
+	modal[0].classList.remove('is-active');
+	html.classList.remove('scroll-hidden');
 }
 
 
 function openModalCheck() {
-	modalCheck[0].classList.add('is-active')
+	modalCheck[0].classList.add('is-active');
+	html.classList.add('scroll-hidden');
 }
 
 function closeModalCheck() {
-	modalCheck[0].classList.remove('is-active')
+	modalCheck[0].classList.remove('is-active');
+	html.classList.remove('scroll-hidden');
 }
